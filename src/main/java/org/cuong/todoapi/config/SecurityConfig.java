906337e024bd5a4df58a8e216b5502af421838c3
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-01-23 17:37:22 +0700

    method level security

---
+++ src/main/java/org/cuong/todoapi/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package org.cuong.todoapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable());
+        http.authorizeHttpRequests(c -> {
+            // c.requestMatchers(HttpMethod.POST,"/api/todos/**").hasRole("ADMIN");
+            // c.requestMatchers(HttpMethod.PUT,"/api/todos/**").hasRole("ADMIN");
+            // c.requestMatchers(HttpMethod.DELETE,"/api/todos/**").hasRole("ADMIN");
+            c.anyRequest().authenticated();
+        });
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    UserDetailsManager userDetailsManager() {
+        var manager = new InMemoryUserDetailsManager();
+        var u1 = User.withUsername("ana")
+                .password("123")
+                .roles("USER")
+                .build();
+        var u2 = User.withUsername("topson")
+                .password("123")
+                .roles("ADMIN")
+                .build();
+
+        manager.createUser(u1);
+        manager.createUser(u2);
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
--- src/main/java/org/cuong/todoapi/controller/TodoController.java
+++ src/main/java/org/cuong/todoapi/controller/TodoController.java
@@ -4,6 +4,7 @@ import org.cuong.todoapi.dto.TodoDto;
 import org.cuong.todoapi.service.TodoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class TodoController {
         this.todoService = todoService;
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<TodoDto> addTodo(@RequestBody TodoDto todoDto) {
         TodoDto saveTodoDto = todoService.addTodo(todoDto);
