f02d2e6b36a299f9396baead6ac126032d788bb7
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-01-23 20:22:32 +0700

    database authentication

--- src/main/java/org/cuong/todoapi/config/SecurityConfig.java
+++ src/main/java/org/cuong/todoapi/config/SecurityConfig.java
@@ -2,22 +2,20 @@ package org.cuong.todoapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-    
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
@@ -26,24 +24,12 @@ public class SecurityConfig {
     }
 
     @Bean
-    UserDetailsManager userDetailsManager() {
-        var manager = new InMemoryUserDetailsManager();
-        var u1 = User.withUsername("ana")
-                .password("123")
-                .roles("USER")
-                .build();
-        var u2 = User.withUsername("topson")
-                .password("123")
-                .roles("ADMIN")
-                .build();
-
-        manager.createUser(u1);
-        manager.createUser(u2);
-        return manager;
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean
     PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/org/cuong/todoapi/repository/UserRepository.java
+++ src/main/java/org/cuong/todoapi/repository/UserRepository.java
@@ -2,11 +2,17 @@ package org.cuong.todoapi.repository;
 
 import org.cuong.todoapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
-    Boolean existsByEmail(String email);
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
+    Boolean existsByEmail(@Param("email") String email);
+
+    Optional<User> findByUsernameOrEmail(String username, String email);
 }
