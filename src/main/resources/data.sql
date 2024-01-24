61ca55d352f8111c006cc46c59d2d46f9adbdce8
Cuong Nguyen <84526626+cuong0411@users.noreply.github.com>
2024-01-24 20:47:33 +0700

    remove db name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ LIMIT 0, 1000
 
 -- Date: 2024-01-23 20:07
 */
-INSERT INTO `todo_list.users` (`id`,`email`,`name`,`password`,`username`) VALUES (1,'ana@og.com','Ana Pham','$2a$12$/bjuHnHJmLX6C3QMFJBHT.yTN2dGrxp4.dVlCq1wlyvdKv.KFP/ye','ana');
-INSERT INTO `todo_list.users` (`id`,`email`,`name`,`password`,`username`) VALUES (2,'topson@og.com','Topson Topias','$2a$12$/bjuHnHJmLX6C3QMFJBHT.yTN2dGrxp4.dVlCq1wlyvdKv.KFP/ye','topson');
+INSERT INTO `users` (`id`,`email`,`name`,`password`,`username`) VALUES (1,'ana@og.com','Ana Pham','$2a$12$/bjuHnHJmLX6C3QMFJBHT.yTN2dGrxp4.dVlCq1wlyvdKv.KFP/ye','ana');
+INSERT INTO `users` (`id`,`email`,`name`,`password`,`username`) VALUES (2,'topson@og.com','Topson Topias','$2a$12$/bjuHnHJmLX6C3QMFJBHT.yTN2dGrxp4.dVlCq1wlyvdKv.KFP/ye','topson');
 
 
 /*
@@ -14,8 +14,8 @@ LIMIT 0, 1000
 
 -- Date: 2024-01-23 20:08
 */
-INSERT INTO `todo_list.roles` (`id`,`name`) VALUES (1,'ROLE_ADMIN');
-INSERT INTO `todo_list.roles` (`id`,`name`) VALUES (2,'ROLE_USER');
+INSERT INTO `roles` (`id`,`name`) VALUES (1,'ROLE_ADMIN');
+INSERT INTO `roles` (`id`,`name`) VALUES (2,'ROLE_USER');
 
 /*
 -- Query: SELECT * FROM todo_list.users_roles
@@ -23,5 +23,5 @@ LIMIT 0, 1000
 
 -- Date: 2024-01-23 20:09
 */
-INSERT INTO `todo_list.users_roles` (`user_id`,`role_id`) VALUES (2,1);
-INSERT INTO `todo_list.users_roles` (`user_id`,`role_id`) VALUES (1,2);
+INSERT INTO `users_roles` (`user_id`,`role_id`) VALUES (2,1);
+INSERT INTO `users_roles` (`user_id`,`role_id`) VALUES (1,2);
